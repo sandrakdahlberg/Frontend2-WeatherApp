62f5111376e3b2320084af542678a5f4f58ba718
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-03-26 10:33:30 +0100

    Ändrade icons

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -6,12 +6,16 @@ export default class WeatherDetails extends Component{
     render()
     {
         let days = this.props.forecast.map((day)=>{
-            let img = day.id;
+            let iconId = day.weather.map((w)=> {
+                return (w.id)
+            });
+            //let iconString = 'owf owf-'+iconId+' owd-5x';
+            let iconString = 'owf owf-'+iconId+' owf-3x owf-pull-left owf-border';
             return(<div>
                     <p>{day.dt_txt}</p>
                     <p>Min: {day.main.temp_min}°C</p>
                     <p>Max: {day.main.temp_max}°C</p> 
-                    <i className="owf owf-800 owd-5x"></i>
+                    <i className={iconString}></i>
                 </div>
             )
         })
