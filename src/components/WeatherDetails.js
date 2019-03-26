d8beddfef80f5f2bf6a31759620eb0116cd798dc
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-03-26 10:22:31 +0100

    La till icons

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
+import '../owfont-master/css/owfont-regular.css';
 
 export default class WeatherDetails extends Component{
 
     render()
     {
         let days = this.props.forecast.map((day)=>{
+            let img = day.id;
             return(<div>
                     <p>{day.dt_txt}</p>
                     <p>Min: {day.main.temp_min}°C</p>
                     <p>Max: {day.main.temp_max}°C</p> 
-                    {/* <i className="owf owf-"{day.weather.id}"owd-5x"></i>                 */}
+                    <i className="owf owf-800 owd-5x"></i>
                 </div>
             )
         })
